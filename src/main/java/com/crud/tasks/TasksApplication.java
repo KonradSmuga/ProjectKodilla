fde229c258e517eb6869636327abddea2bb7ea4f
KonradSmuga <smuggk@gmail.com>
2018-03-05 20:45:23 +0100

    delete error

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TasksApplication {
 
 	public static void main(String[] args) {
-
-	    TaskDto taskDto = new TaskDto();
 		SpringApplication.run(TasksApplication.class, args);
 	}
 }
