40b786114c39f78b85ffd3272c128b7a3d3fb4ef
KonradSmuga <smuggk@gmail.com>
2018-04-15 19:34:08 +0200

    update on Heroku

--- src/main/java/com/crud/tasks/domain/Trello.java
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -6,10 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
 @AllArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 @NoArgsConstructor
+
 public class Trello {
 
     @JsonProperty("trello")
