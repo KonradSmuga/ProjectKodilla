43725cea3f39e02d1ebd77ab89bd015558737983
KonradSmuga <smuggk@gmail.com>
2018-04-16 15:40:53 +0200

    19.4 end of task mailer

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -20,9 +20,12 @@ public class EmailScheduler {
     @Autowired
     private AdminConfig adminConfig;
 
-    @Scheduled(fixedDelay =  10000)
+    @Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
-        simpleEmailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT, "Currently in database you got : " + size + " tasks", ""));
+        if (size != 1) {
+            simpleEmailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT, "Currently in database you got : " + size + " tasks", ""));
+        }
+            simpleEmailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT, "Currently in database you got : " + size + " task", ""));
     }
 }
