c9a7cb60c3da3ac17364ce67cfe20ff799b0a5ff
KonradSmuga <smuggk@gmail.com>
2018-05-08 16:41:47 +0200

    21.3 TaskController test

--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -6,6 +6,7 @@ import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloListDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
+import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +23,11 @@ import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -43,31 +45,46 @@ public class TaskControllerTest {
     @MockBean
     private DbService dbService;
 
-
     @Test
-    public void shouldGetTasks() throws Exception{
+    public void shouldFetchEmptyList() throws Exception {
         //Given
-
+        List<TaskDto> taskDto = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
+        when(taskMapper.mapToTaskDtoList(anyList())).thenReturn(taskDto);
+        when(dbService.getAllTasks()).thenReturn(tasks);
 
         //When & Then
-
-    }
-
-
-
-    @Test
-    public void getTask1() {
+        mockMvc.perform(get("/v1/task/getTask"))
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$", hasSize(0)));
     }
 
     @Test
-    public void deleteTask() {
+    public void shouldFetchTask() throws Exception{
+        //Given
+        TaskDto taskDto = new TaskDto(1L, "title", "content");
+        when(taskMapper.mapToTaskDto(any(Task.class))).thenReturn(taskDto);
+        when(dbService.findById(1L)).thenReturn(Optional.of(new Task()));
+
+        //When & Then
+        mockMvc.perform(get("/v1/task/getTask").contentType(MediaType.APPLICATION_JSON)
+                .param("taskId","1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id",is(1)))
+                .andExpect(jsonPath("$.title",is("content")));
     }
 
     @Test
-    public void updateTask() {
-    }
-
-    @Test
-    public void createTask() {
+    public void shouldCreateTask() throws Exception{
+        //Given
+        TaskDto taskDto = new TaskDto(1l, "title", "content");
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(taskDto);
+        System.out.println(jsonContent);
+        //When & Then
+        mockMvc.perform(post("/v1/task/createTask").contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(jsonContent))
+                .andExpect(status().is(200));
     }
 }
