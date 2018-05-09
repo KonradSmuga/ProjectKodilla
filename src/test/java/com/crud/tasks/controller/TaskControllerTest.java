9d96f5dc8c1caecf0d2bcae3733f53cae10c0d41
KonradSmuga <smuggk@gmail.com>
2018-05-09 16:27:44 +0200

    21.4 Actuator

--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -32,8 +32,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(TaskController.class)
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(TaskController.class)
 public class TaskControllerTest {
 /*
     @Autowired
