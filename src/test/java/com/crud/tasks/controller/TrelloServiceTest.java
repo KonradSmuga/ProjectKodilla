c1783334e889883a4c22ed9cf2b7a26db7d5cf09
KonradSmuga <smuggk@gmail.com>
2018-05-09 22:36:51 +0200

    21.3 fixed tests

--- src/test/java/com/crud/tasks/controller/TrelloServiceTest.java
+++ src/test/java/com/crud/tasks/controller/TrelloServiceTest.java
@@ -1,19 +1,6 @@
 package com.crud.tasks.controller;
 
-import com.crud.tasks.domain.TrelloBoardDto;
-import com.crud.tasks.trello.client.TrelloClient;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
-
-@RunWith(MockitoJUnitRunner.class)
 public class TrelloServiceTest {
 
 
